3c52b9780f66c651a67b40f2409020bb837734a7
Meghanadhanani <123074080+Meghanadhanani@users.noreply.github.com>
2024-06-13 16:51:35 +0530

    Exracted Data from DB in mostrated page

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,13 +1,24 @@
-import { db } from "../../firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+// // src/app/api/user/route.js
 
-export async function GET(req, res) {
-  try {
-    const querySnapshot = await getDocs(collection(db, "products"));
-    const products = querySnapshot.docs.map((doc) => doc.data());
-    return res.status(200).json(products);
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    return res.status(500).json({ error: "Error fetching products" });
-  }
+// import { collection, getDocs } from 'firebase/firestore';
+// import { db } from '../../firebaseConfig'; // Adjust path as per your project structure
+
+// export default async function handler(req, res) {
+//   if (req.method === 'GET') {
+//     try {
+//       const usersCollection = collection(db, 'users');
+//       const snapshot = await getDocs(usersCollection);
+//       const users = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+//       res.status(200).json(users);
+//     } catch (error) {
+//       console.error('Error fetching users: ', error);
+//       res.status(500).json({ error: 'Failed to fetch users' });
+//     }
+//   } else {
+//     res.status(405).json({ error: 'Method Not Allowed' });
+//   }
+// }
+
+export async function GET(reques) {
+  return Response("uSer")
 }
